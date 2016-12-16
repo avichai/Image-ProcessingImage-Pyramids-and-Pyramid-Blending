b92d01807adde025f1da38abe31c28771126234d
avichai <avichai.bendavid@mail.huji.ac.il>
2016-12-16 20:25:29 +0200

    few functions and q1

--- ex3/sol3.py
+++ ex3/sol3.py
@@ -1,5 +1,4 @@
 __author__ = 'Avichai'
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.misc import imread as imread
@@ -8,30 +7,25 @@ from scipy import signal as sig
 from scipy.ndimage.filters import convolve
 import os
 
-
-
 IDENTITY_KERNEL_SIZE = 1
 BINOMIAL_MAT = [0.5, 0.5]
-
 GRAY = 1
 RGB = 2
 NORM_PIX_FACTOR = 255
+ROWS = 0
+COLS = 1
+LARGEST_IM_INDEX = 0
 
-MIN_DIM_PYR = 16
 
 
-
-
-#todo not in use for noe
+# todo not in use for noe
 
 IMAGE_DIM = 2
 DER_VEC = [1, 0, -1]
 DIM_RGB = 3
 MAX_PIX_VALUE = 256
 MIN_PIX_VALUE = 0
-Y = 0
-ROWS = 0
-COLS = 1
+
 
 def read_image(filename, representation):
     """this function reads a given image file and converts it into a given
@@ -66,11 +60,12 @@ def imdisplay(filename, representation):
                      RGB image (2)."""
     im = read_image(filename, representation)
     plt.figure()
-    if(representation == GRAY):
+    if (representation == GRAY):
         plt.imshow(im, cmap=plt.cm.gray)
     else:
         plt.imshow(im)
     plt.show(block=True)
+
 
 def getGaussVec(kernel_size):
     '''
@@ -80,27 +75,69 @@ def getGaussVec(kernel_size):
     '''
     if kernel_size == IDENTITY_KERNEL_SIZE:
         return [1]
-    return sig.convolve(BINOMIAL_MAT, getGaussVec(kernel_size-1))
+    return sig.convolve(BINOMIAL_MAT, getGaussVec(kernel_size - 1))
 
 
-def getGaussMat(kernel_size):
+def getImAfterBlur(im, Filter, filter_size):
     '''
-    getting a gaussian kernel of size kernel_sise^2
-    :param kernel_size: the size of the wished kernel in
-    each dimension (an odd integer)
-    :return: gaussian kernel of size kernel_sise^2
+    return the image after row and col blur
+    :param im: the image to blur
+    :param Filter: the filter to blur with
+    :return: blurred image
     '''
-    '''geeting the blure vec in 1d'''
-    gaussVec = getGaussVec(kernel_size)
-    '''creating the 2d kernel'''
-    gaussAsMat = np.array(gaussVec)
-    gaussMat = sig.convolve2d(gaussAsMat.reshape(kernel_size, 1),
-                             gaussAsMat.reshape(1, kernel_size))
-    return gaussMat
+    filterAsMat = np.array(Filter).reshape(1, filter_size)
+    # todo choose which convolve do I prefer
+    # blurXIm = convolve(im, filterAsMat, mode='reflect')
+    blurXIm = convolve(im, filterAsMat, mode='constant', cval=0.0)
+    # blurIm = convolve(blurXIm, filterAsMat.transpose(), mode='reflect')
+    blurIm = convolve(blurXIm, filterAsMat.transpose(),
+                      mode='constant', cval=0.0).astype(np.float32)
+    return blurIm
 
-def reduceIm(currIm, gaussFilter):
-    blurIm = convolv(currIm, gaussFilter)
-    pass
+
+def reduceIm(currIm, gaussFilter, filter_size):
+    '''
+    reduce an image
+    :param currIm: the image to reduce by 4
+    :param gaussFilter: the filter to blur with the image before reduce
+    :param filter_size: the size of the filter
+    :return: the reduced image
+    '''
+    blurIm = getImAfterBlur(currIm, gaussFilter, filter_size)
+    reducedImage = blurIm[::2, ::2]
+    return reducedImage.astype(np.float32)
+
+
+def expandIm(currIm, gaussFilterForExpand, filter_size):
+    '''
+    expand an image
+    :rtype : np.float32
+    :param currIm: the image to expand by 4
+    :param gaussFilterForExpand: the filter to blur with the expand image
+    :param filter_size: the size of the filter
+    :return: an expand image
+    '''
+    expandImage = np.zeros((2 * currIm.shape[0], 2 * currIm.shape[1]))
+    expandImage[::2, ::2] = currIm
+    expandRes = getImAfterBlur(expandImage, gaussFilterForExpand, filter_size)
+    return expandRes.astype(np.float32)
+
+
+def getNumInInPyr(im, max_levels):
+    '''
+    return maximum number of images in pyramid
+    :param im: tne original image
+    :param max_levels: an initial limitation
+    :return: the real limitation
+    '''
+    numRows = im.shape[ROWS]
+    numCols = im.shape[COLS]
+
+    limRows = np.floor(np.log2(numRows)) - 3
+    limCols = np.floor(np.log2(numCols)) - 3
+    numImInPyr = np.uint8(np.min([max_levels, limCols, limRows]))
+    return numImInPyr
+
 
 def build_gaussian_pyramid(im, max_levels, filter_size):
     '''
@@ -110,17 +147,17 @@ def build_gaussian_pyramid(im, max_levels, filter_size):
     :param filter_size: the size of the Gaussian filter
     :return: Gaussian pyramid as standard python array
     '''
-    gaussFilter = getGaussMat(filter_size)
+    numImInPyr = getNumInInPyr(im, max_levels)
+    gaussFilter = getGaussVec(filter_size)
+
     gaussPyr = [im]
     currIm = im
 
     # todo add braking when size of im is less than 16 pix
-    for i in range (max_levels):
-        if currIm.shape[0]//2 < MIN_DIM_PYR or currIm.shape[1]//2 < MIN_DIM_PYR:
-            break
-        currIm = reduceIm(currIm, gaussFilter)
-        gaussPyr = gaussPyr + currIm
-    pass
+    for i in range(1, numImInPyr):
+        currIm = reduceIm(currIm, gaussFilter, filter_size)
+        gaussPyr.append(currIm)
+    return gaussPyr, gaussFilter
 
 
 def build_laplacian_pyramid(im, max_levels, filter_size):
@@ -129,6 +166,88 @@ def build_laplacian_pyramid(im, max_levels, filter_size):
     :param im: a grayscale image with double values in [0, 1]
     :param max_levels: the maximal number of levels in the resulting pyramid
     :param filter_size: the size of the Gaussian filter
-    :return: Laplacian pyramid as standard python array
+    :return: Laplacian pyramid as standard python array and the filter vec
     '''
-    pass
+    gaussFilter = getGaussVec(filter_size)
+    laplacianPyr = []
+
+    gaussPyr = build_gaussian_pyramid(im, max_levels, filter_size)[0]
+    numImInPyr = len(gaussPyr)
+
+    for i in range(numImInPyr - 1):
+        laplacianPyr.append(gaussPyr[i] - expandIm(
+            gaussPyr[i + 1], 2 * gaussFilter, filter_size))
+    laplacianPyr.append(gaussPyr[numImInPyr - 1])
+    return laplacianPyr, gaussFilter
+
+
+def laplacian_to_image(lpyr, filter_vec, coeff):
+    '''
+    reconstruction of an image from its Laplacian Pyramid
+    :param lpyr: Laplacian pyramid
+    :param filter_vec: the filter that was used in order to
+            construct the pyramid
+    :param coeff: the coefficient of each image in the pyramid
+    :return: reconstruction of an image from its Laplacian Pyramid
+    '''
+    numIm = len(lpyr)
+    numCoe = len(coeff)
+    if numIm != numCoe:
+        '''invalid input'''
+        return
+    gni = lpyr[numIm - 1]
+    for i in range(numIm - 1):
+        gni = expandIm(gni, 2 * filter_vec, len(filter_vec)) + (
+            lpyr[numIm - 1 - i - 1] * coeff[len(coeff) - 1 - i])
+    return gni.astype(np.float32)
+
+
+def strechIm(im, newMin, newMax):
+    """
+    strech the image to [newMin, newMax]
+    :param newMax: max vlue to stretch to
+    :param newMin: min value to stretch to
+    :param im: float 32 image
+    :return: stretched im
+    """
+    inMin = np.min(im)
+    inMax = np.max(im)
+    stretchedIm = (im - inMin) * ((newMax - newMin) / (inMax - inMin)) + newMin
+    return stretchedIm
+
+
+def render_pyramid(pyr, levels):
+    '''
+    creates a single black image in which the pyramid levels of the
+        given pyramid pyr are stacked horizontally
+    :param pyr: either a Gaussian or Laplacian pyramid
+    :param levels: number of levels to present in the result
+    :return: single black image in which the pyramid levels of the
+        given pyramid pyr are stacked horizontally
+    '''
+    levels = min(levels, len(pyr))
+    numRows = pyr[LARGEST_IM_INDEX].shape[ROWS]
+    numCols = pyr[LARGEST_IM_INDEX].shape[COLS]
+    for i in range(1, levels):
+        numCols += pyr[i].shape[COLS]
+    pyrIm = np.zeros([numRows, numCols])
+    curPlace = 0
+    for i in range(levels):
+        stretchedIm = strechIm(pyr[i], 0, 1)
+        rows = stretchedIm.shape[ROWS]
+        cols = stretchedIm.shape[COLS]
+        pyrIm[:rows, curPlace:curPlace + cols] = stretchedIm
+        curPlace = curPlace + cols
+    return pyrIm.astype(np.float32)
+
+
+def display_pyramid(pyr, levels):
+    '''
+    display at max the amount of levels from the pyr
+    :param pyr: either a Gaussian or Laplacian pyramid
+    :param levels: number of levels to present in the result
+    '''
+    res = render_pyramid(pyr, levels)
+    plt.figure()
+    plt.imshow(res, cmap=plt.cm.gray)
+    plt.show(block=True)
--- ex3/testSol3.py
+++ ex3/testSol3.py
@@ -1,27 +1,104 @@
 from ex3 import sol3
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 def testGaussPyr():
+    NUM_PIC = 4
     im = sol3.read_image('external/jerusalem.jpg', 1)
-    sol3.imdisplay('external/jerusalem.jpg', 1)
-    # pyr, filter_vec = sol3.build_gaussian_pyramid(im, max_levels, filter_size)
-    # pyr, filter_vec = build_laplacian_pyramid(im, max_levels, filter_size)
+    # sol3.imdisplay('external/jerusalem.jpg', 1)
+    im = im[:2 ** (np.uint(np.floor(np.log2(im.shape[0])))),
+         :2 ** (np.uint(np.floor(np.log2(im.shape[1]))))]
+    pyr, filter_vec = sol3.build_gaussian_pyramid(im, NUM_PIC, 11)
+    print(filter_vec)
+
+    plt.figure()
+    for i in range(len(pyr)):
+        plt.subplot(1, NUM_PIC, 1 + i)
+        plt.imshow(pyr[i], cmap=plt.cm.gray)
+    plt.show(block=True)
 
 
+def testLapPyr():
+    NUM_PIC = 7
+    im = sol3.read_image('external/jerusalem.jpg', 1)
+    im = im[:2 ** (np.uint(np.floor(np.log2(im.shape[0])))),
+         :2 ** (np.uint(np.floor(np.log2(im.shape[1]))))]
+    pyr, filter_vec = sol3.build_laplacian_pyramid(im, NUM_PIC, 3)
+    print(filter_vec)
+    plt.figure()
+    for i in range(len(pyr)):
+        plt.subplot(1, NUM_PIC, 1 + i)
+        plt.imshow(pyr[i], cmap=plt.cm.gray)
+    plt.show(block=True)
 
 
+def testReconstract():
+    NUM_PIC = 7
+    # im = sol3.read_image('external/jerusalem.jpg', 1)
+    im = sol3.read_image('external/monkey.jpg', 1)
+    # im = sol3.read_image('external/LowContrast.jpg', 1)
+    im = im[:2 ** (np.uint(np.floor(np.log2(im.shape[0])))),
+         :2 ** (np.uint(np.floor(np.log2(im.shape[1]))))]
+    lpyr, filter_vec = sol3.build_laplacian_pyramid(im, NUM_PIC, 11)
+    img = sol3.laplacian_to_image(lpyr, filter_vec, [1, 1, 1, 1, 1])
+
+    plt.figure()
+    plt.imshow(im, cmap=plt.cm.gray)
+    plt.figure()
+    plt.imshow(img, cmap=plt.cm.gray)
+
+    print(np.mean(np.abs(img - im)))
+    plt.show(block=True)
 
 
+def testRenderPyr():
+    NUM_PIC = 5
+    im = sol3.read_image('external/jerusalem.jpg', 1)
+    # sol3.imdisplay('external/jerusalem.jpg', 1)
+    im = im[:2 ** (np.uint(np.floor(np.log2(im.shape[0])))),
+         :2 ** (np.uint(np.floor(np.log2(im.shape[1]))))]
+    pyrg, filter_vec1 = sol3.build_gaussian_pyramid(im, NUM_PIC, 11)
+    pyrl, filter_vec2 = sol3.build_laplacian_pyramid(im, NUM_PIC, 11)
+
+    resg = sol3.render_pyramid(pyrg, len(pyrg))
+    plt.figure()
+    plt.imshow(resg, cmap=plt.cm.gray)
+
+    resl = sol3.render_pyramid(pyrl, 10)
+    plt.figure()
+    plt.imshow(resl, cmap=plt.cm.gray)
+    plt.show(block=True)
+    # print(resl)
 
 
+def testDispPyr():
+    NUM_PIC = 5
+    im = sol3.read_image('external/jerusalem.jpg', 1)
+    # sol3.imdisplay('external/jerusalem.jpg', 1)
+    im = im[:2 ** (np.uint(np.floor(np.log2(im.shape[0])))),
+         :2 ** (np.uint(np.floor(np.log2(im.shape[1]))))]
+    pyrg, filter_vec1 = sol3.build_gaussian_pyramid(im, NUM_PIC, 11)
+    pyrl, filter_vec2 = sol3.build_laplacian_pyramid(im, NUM_PIC, 11)
+    sol3.display_pyramid(pyrg, 10)
+    sol3.display_pyramid(pyrl, 10)
+
+
+# tests:
+# testGaussPyr
+# testLapPyr
+# testReconstract
+# testRenderPyr
+# testDispPyr
 
 def main():
     try:
-        for test in [testGaussPyr]:
+        for test in [testDispPyr]:
             test()
     except Exception as e:
         print("Failed test due to: {0}".format(e))
         exit(-1)
+
 
 if __name__ == '__main__':
     main()
